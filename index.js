20fc2cae4425c62df226d033b6cee3241c226490
shauncoding28 <196330731+shauncoding28@users.noreply.github.com.>
2025-02-18 11:39:28 +0100

    add cors and error handling middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
@@ -12,6 +13,7 @@ require('dotenv').config();
 
 
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
@@ -31,6 +33,17 @@ app.get('/users/:id', db.getUserById);
 app.post('/users', db.createUser);
 app.put('/users/:id', db.updateUser);
 app.delete('/users/:id', db.deleteUser);
+
+
+app.use((req, res, next) => {
+    const error = new Error('Something went wrong');
+    next(error);
+});
+// Error-handling Middleware
+app.use((err, req, res, next) => {
+    console.error('Error:', err.message);
+    res.status(500).send('Internal Server Error');
+});
 
 
 
